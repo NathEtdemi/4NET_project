d537b0d6d97f520bec04f02a09caa167f85e745d
Nathim <nathimrichar@gmail.com>
2024-03-20 12:15:24 +0100

    finition projet

--- Shared/FormModels/VehicleFormModel.cs
+++ Shared/FormModels/VehicleFormModel.cs
@@ -10,20 +10,21 @@ namespace Shared.FormModels
 {
 	public class VehicleFormModel
 	{
-		public int Id { get; set; }
+        public int Id { get; set; }
 
-		[Required(ErrorMessage = "Le champ est requis")]
-		public int CarModelId { get; set; }
+        [Required(ErrorMessage = "Le champ est requis")]
+        [Range(1, int.MaxValue, ErrorMessage = "Veuillez sélectionner un modèle de voiture")]
+        public int CarModelId { get; set; }
 
-		[Required(ErrorMessage = "Le champ Immatriculatino est requis!")]
-		[StringLength(9, MinimumLength = 7, ErrorMessage = "L'immatriculation doit être comprise entre 7 et 9 caractères")]
-		public string NumberPlate { get; set; }
+        [Required(ErrorMessage = "Le champ Immatriculation est requis")]
+        [StringLength(9, MinimumLength = 7, ErrorMessage = "L'immatriculation doit être comprise entre 7 et 9 caractères")]
+        public string NumberPlate { get; set; }
 
-		public int BuildYear { get; set; }
+        public int BuildYear { get; set; }
 
-		[Range(0, 999999, ErrorMessage = "La valeur des km doit être comprise entre 0 et 999 999 km")]
-		public int KmNumber { get; set; }
+        [Range(0, 999999, ErrorMessage = "La valeur des km doit être comprise entre 0 et 999 999 km")]
+        public int KmNumber { get; set; }
 
-		public Energy EnergySource { get; set; }
-	}
+        public Energy EnergySource { get; set; }
+    }
 }
